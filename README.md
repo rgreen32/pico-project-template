cce25ef7a3e8e7bc995679680d8accc9f4897fe2
rgreen32 <ray.greenm@gmail.com>
2023-07-29 13:31:48 -0400

    added starter code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# pico-bluetooth
+# pico-project-template
+
+## Pre-requisites
+Download pico-sdk and set the env variable to its path: export PICO_SDK=/Path/to/pico-sdk
---
+++ blink/blink.c
@@ -0,0 +1,33 @@
+#define BOARD PICO_W //Change the value of board to the kind of pico you're compiling to
+
+#if BOARD != PICO_W
+
+#include "pico/cyw43_arch.h"
+int main() {
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+    }
+}
+#else
+
+#include "pico/stdlib.h"
+int main() {
+    stdio_init_all();
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return -1;
+    }
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+    }
+}
+#endif
